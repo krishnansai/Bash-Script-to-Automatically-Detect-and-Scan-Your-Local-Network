e6f944f0eeb73260f9bcaeeb42bd0a6bf89b452b
Gokula Krishnan.K <65504920+krishnansai@users.noreply.github.com>
2022-01-12 00:11:42 +0530

    Add files via upload

---
+++ network_scan.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/bash
+
+echo " Scan The Local Network And Open Ports "
+
+#get the octets ip 
+
+ifconfig | grep "broadcast" | cut -d " " -f 10 | cut -d "." -f 1,2,3 | grep "192" | uniq > octat.txt
+
+#ping comments
+
+OCTATS=$(cat octat.txt)
+
+#create a  new .txt
+echo "" > $OCTATS.txt
+
+#loop
+for ip in {1..254}
+do
+	ping -c 1 $OCTATS.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" >> $OCTATS.txt &
+done
+
+cat $OCTATS.txt
+
+#nmap scan
+#sudo apt-get install nmap
+
+nmap -sS -iL $OCTATS.txt
+
+exit
