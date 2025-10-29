8828d4b30750b9dd001cab7246055a1f3be9f5e8
Gokula Krishnan.K <65504920+krishnansai@users.noreply.github.com>
2025-10-29 20:00:42 +0530

    Script

--- network_scan.sh
+++ network_scan.sh
@@ -4,26 +4,26 @@ echo " Scan The Local Network And Open Ports "
 
 #get the octets ip 
 
-ifconfig | grep "broadcast" | cut -d " " -f 10 | cut -d "." -f 1,2,3 | grep "192" | uniq > octat.txt
+#ifconfig | grep "broadcast" | cut -d " " -f 10 | cut -d "." -f 1,2,3 | grep "192" | uniq > octat.txt
 
 #ping comments
 
-OCTATS=$(cat octat.txt)
+#OCTATS=$(cat octat.txt)
 
 #create a  new .txt
 echo "" > $OCTATS.txt
 
 #loop
-for ip in {1..254}
-do
-	ping -c 1 $OCTATS.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" >> $OCTATS.txt &
-done
+#for ip in {1..254}
+#do
+#	ping -c 1 $OCTATS.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" >> $OCTATS.txt &
+#done
 
-cat $OCTATS.txt
+#cat $OCTATS.txt
 
 #nmap scan
 #sudo apt-get install nmap
 
-nmap -sS -iL $OCTATS.txt
+nmap -vvv localhost > result.sh
 
-exit
+#exit
